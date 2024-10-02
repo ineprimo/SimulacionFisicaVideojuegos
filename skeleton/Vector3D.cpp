7e27284615fa2bbffe20edf80e3bd1645b5b1ee7
(i)nés <ineprimo@ucm.es>
2024-10-02 17:43:58 +0200

    ugu

--- skeleton/Vector3D.cpp
+++ skeleton/Vector3D.cpp
@@ -19,21 +19,4 @@ float Vector3D::module(Vector3D vector)
     return mod;
 }
 
-Vector3D& Vector3D::operator*(Vector3D& vec)
-{
-    // TODO: Insertar una instrucción "return" aquí
-}
-
-float& Vector3D::operator*(float& a)
-{
-
-}
-
-float& Vector3D::operator*(Vector3D& vec) const
-{
-    // TODO: Insertar una instrucción "return" aquí
-    float a = vec.x() * x();
-    return (vec.x() + vec.y());
-}
-
 
