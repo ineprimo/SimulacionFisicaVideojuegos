000ba857021b33df2e593dfda0aa4ddc2a51ca5b
ineprimo <ineprimo@ucm.es>
2024-11-19 19:01:49 +0100

    tecnicamente la explosion esta lmao

--- skeleton/ExplosionGenerator.cpp
+++ skeleton/ExplosionGenerator.cpp
@@ -23,15 +23,14 @@ Vector3 ExplosionGenerator::force(double t, Particle* p)
 {
 	Vector3 f(0, 0, 0);
 
-	std::cout << t << std::endl;
 
-	std::cout << "bomba" << std::endl;
 	// distancia al centro de la explosion
 	float dist = (p->getPosition() - position).magnitude();
 	// si la distancia es menor que el radio la fuerza es 0
-	if (dist >= radio) return f; 
+	//if (dist >= radio) return f; 
+	//std::cout << "bomba" << std::endl;
 
-	f = ((k / pow(dist, 2)) * (p->getPosition() - position)) * exp(-time / tau);
-
+	f = ((k / radio* radio) * (p->getPosition() - position)) * exp(-t / tau);
+	//f = ((k / radio * radio) * (p->getPosition() - position)) * exp(-t / tau);
 	return f;
 }
