697d86902547b771426ff4799ee9a0118fa771ed
ineprimo <ineprimo@ucm.es>
2024-11-27 22:56:05 +0100

    hm

--- skeleton/SpringForceGenerator.cpp
+++ skeleton/SpringForceGenerator.cpp
@@ -33,7 +33,6 @@ Vector3 SpringForceGenerator::force(double t, Particle* p)
 	// distancia
 	Vector3 d = anchor->getPosition() - p->getPosition();
 	float l = d.magnitude();
-
 	const float delta = d.normalize() - rest;
 	//const float delta = max(0.0f, d.normalize() - rest);
 
